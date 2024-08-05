89836ee4688c357c915500a43a90d9b6ae324f08
ScriptSlingerAlok <hackingallthings538@gmail.com>
2024-08-06 02:05:18 +0530

    Initial Commit

--- 01-Fibonacci Series/01-Fibonacci series.cpp
+++ 01-Fibonacci Series/01-Fibonacci series.cpp
@@ -5,7 +5,8 @@ int main ()
 {
 	int i,n,t1=0,t2=1,nextterm;
 	printf("Enter the number of terms: ");
-	scanf("Fibonacci Series: ");
+	scanf("%d",&n);
+	printf("Fibonacci Series: ");
 	for(i=1;i<=n;++i)
 	{
 		printf("%d",t1);
